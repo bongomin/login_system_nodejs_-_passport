32c6781c28f4b660bd9d12f15468ed110ac4b9fb
bongomin <bongomindaniel@gmail.com>
2019-09-11 20:17:15 +0300

    feat(added mlab altlas database string ) : connecting to mongodb database in the cloud.

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 if(process.env.NODE_ENV === 'production'){
-   // connection to local db
+  
  // connecting to cloud db
- module.exports ={mongoURI: '' 
+ module.exports ={mongoURI: 'mongodb+srv://danny:P@55w0rd55@loginapp-klkrl.mongodb.net/test?retryWrites=true&w=majority' 
 }
   
 }else{
+    // connection to local db
    module.exports = {mongoURI :'mongodb://localhost/loginSystem'}
 }
--- helpers/auth.js
+++ helpers/auth.js
@@ -7,3 +7,4 @@ module.exports = {
      res.redirect('/');
    }
  }
+
